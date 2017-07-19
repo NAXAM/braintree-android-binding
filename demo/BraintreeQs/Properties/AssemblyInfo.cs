71779f3d73e3fa12b219a546a18172d6255cd41a
Vu Duc Tuyen <tuyen@naxam.net>
2017-07-19 10:36:03 +0700

    #1 - Missing Google Wallet dependency

--- demo/BraintreeQs/Properties/AssemblyInfo.cs
+++ demo/BraintreeQs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Naxam.BrainTree.Droid")]
-[assembly: AssemblyDescription("Xamarin Binding Library - BrainTree Android SDK")]
+[assembly: AssemblyTitle("BraintreeQs")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
-[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
 [assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.5.3")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
