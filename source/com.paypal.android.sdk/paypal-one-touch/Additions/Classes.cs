818c57dd221651a24d45dd5ca794ac0233523b17
Vũ Đức Tuyến <tuyen.vuduc@signkeys.com>
2020-06-29 14:08:56 +0700

    Braintree v3.11.0

---
+++ source/com.paypal.android.sdk/paypal-one-touch/Additions/Classes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Com.Paypal.Android.Sdk.Onetouch.Core.Config
+{
+    partial class BillingAgreementRecipe
+    {
+        protected override global::Java.Lang.Object RawThis
+        {
+            get {
+                return This;
+            }
+        }
+    }
+
+	partial class CheckoutRecipe
+	{
+		protected override global::Java.Lang.Object RawThis
+		{
+			get
+			{
+				return This;
+			}
+		}
+	}
+
+	partial class OAuth2Recipe
+	{
+		protected override global::Java.Lang.Object RawThis
+		{
+			get
+			{
+				return This;
+			}
+		}
+	}
+}
